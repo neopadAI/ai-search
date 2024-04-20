ec8f29f10becc33233960a3f3ab8d477651d2967
Neopad__ <164239736+neopadAI@users.noreply.github.com>
2024-04-20 11:48:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,9 @@ import ai_search._2_0.main as ai
 
 ai.req = input('ai-search: ')
 ai.run()
-print(ai.result)
+results = ai.result
+
+print('results: ')
+for result in results:
+    print(result)
+    print()
