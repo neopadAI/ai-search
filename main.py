b10535fffd29b9f6c7aeaf122d146b80b54e12ac
Neopad__ <164239736+neopadAI@users.noreply.github.com>
2025-04-18 19:48:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 # to-train
-import to_train.main
+import to_train.main as train
+
+main_folder = "books"
+
+category_name = input("Category: ")
+category_folder_path = train.create_folder_if_not_exists(main_folder, category_name)
+
+text = input("Content: ")
+html_file_path = train.unique_file_name(category_folder_path)
+
+train.save_text_to_file(html_file_path, text)
+
 
 # request
 import ai_search.main as ai
